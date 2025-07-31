5c76d5ee14dd7f36d465c88fc269a94c0811a21e
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-31 09:18:15 -0400

    Removes hard-coded baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-baseURL: 'https://eventify-g0fv.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: false,
 })
 
